dd0a43582b7ab6c8fec3346ea2e9d16219c8fdb5
lingineni555 <lingineni555@gmail.com>
2023-11-18 14:58:28 +0000

    Added code to get six months back data and one month back data

--- my_list.py
+++ my_list.py
@@ -4,3 +4,7 @@ historical_data = stock.history(period="15mo")
 #historical_data.head(10)
 current_value = historical_data.iloc[-1]["Close"]
 current_date = historical_data.iloc[-1]["Date"]
+six_months_back_close = historical_data.iloc[0]["Close"]
+six_months_back_date = historical_data.iloc[0]["Date"]
+one_month_back_close = historical_data.iloc[-20]["Close"]  # Approx. 1 month data
+one_month_back_date = historical_data.iloc[-20]["Date"]
