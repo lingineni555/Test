4834a3e9a1bbeb45f5af4717fd740289a7d860c6
lingineni555 <lingineni555@gmail.com>
2023-11-18 14:52:12 +0000

    Added code to get the most recent stock value of apple

--- my_list.py
+++ my_list.py
@@ -1,4 +1,6 @@
-import yfinance as yf 
-stock = yf.Ticker("AAPL") 
-historical_data = stock.history(period="15mo") 
-historical_data.head(10) 
+import yfinance as yf
+stock = yf.Ticker("AAPL")
+historical_data = stock.history(period="15mo")
+#historical_data.head(10)
+current_value = historical_data.iloc[-1]["Close"]
+current_date = historical_data.iloc[-1]["Date"]
